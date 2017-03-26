f6085555f4d8abef370957f0518a0470822fa353
subodh-dharma <subodh.dharma@gmail.com>
2017-03-26 19:16:44 +0000

    file to extract links from files in directory

--- src/extract_links.py
+++ src/extract_links.py
@@ -8,39 +8,31 @@ git_token = os.environ['GITTOKEN']
 gitapi = GitHub(access_token=git_token)
 
 files = os.listdir('../dockerlibrary')
-
-fo = open('../dockerlibrary/adminer')
-line = fo.readline()
-while line:
- if 'GitRepo' in line:
-  print line
-  splitlist = line.split('/',5)
-  repo_owner = splitlist[3]
-  repo_name = splitlist[4].split('.',1)[0]
-  
-
- if 'Directory' in line:
-  #print line
-  tag = (line.split(' ', 1)[1]).rstrip()
-  file_path = '/' + tag + '/Dockerfile'
-  file_git = gitapi.repos(repo_owner)(repo_name).contents(file_path).get()
-  download_file = urllib.URLopener()
-  download_file.retrieve(file_git.download_url, './downloads/Dockerfile_'+repo_name+'_'+tag.replace('/','_'))
-  print file_path 
+count = 0
+for f in files:
+ fo = open('../dockerlibrary/'+f)
+ print fo.name
  line = fo.readline()
-fo.close()
+  
+ while line:
+  if 'GitRepo' in line:
+   print line
+   splitlist = line.split('/',5)
+   repo_owner = splitlist[3]
+   repo_name = splitlist[4].split('.',1)[0]
+ 
+  if 'Directory' in line:
+   #print line   
+   tag = (line.split(' ', 1)[1]).rstrip()
+   file_path = '/' + tag + '/Dockerfile'
+   print file_path
+   print repo_owner, repo_name
+   file_git = gitapi.repos(repo_owner)(repo_name).contents(file_path).get()
+   download_file = urllib.URLopener()
+   download_file.retrieve(file_git.download_url, './downloads/Dockerfile_'+repo_name+'_'+tag.replace('/','_'))
+   count = count + 1
+   print count
 
-#for file in files:
-# print(file)
-# fo = open('../dockerlibrary/'+file, "r")
-# str = fo.read()
-# print str
-# fo.close()
-
-#git_user = 'subodh-dharma'
-
-
-file_name = gitapi.repos('TimWolla')('docker-adminer').contents('/4.3/Dockerfile').get();
-#print file_name.download_url
-#download_file = urllib.URLopener()
-#download_file.retrieve(file_name.download_url, 'Dockerfile_docker-adminer')
+  line = fo.readline()
+  
+ fo.close()
