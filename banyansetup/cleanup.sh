b6722e93043c4f639184abc8b02c2e369f4e744d
subodh-dharma <subodh.dharma@gmail.com>
2017-04-12 02:15:52 +0000

    repo restructuring part 2

--- banyansetup/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#var=`docker images|grep $1|tr -s ' '|cut -d ' ' -f3`
-#echo $var
-docker rmi $1
-mkdir -p $PWD/final_output/$1
-
-cp -r $PWD/test_env/$1/output $PWD/final_output/$1
-rm -rf $PWD/test_env/$1
--- src/community-images/Dockerfile_matthewgall_dnsjson.com
+++
@@ -1,9 +0,0 @@
-FROM matthewgall/python-dev:latest
-
-WORKDIR /app
-
-COPY . /app
-RUN virtualenv /env && /env/bin/pip install -r /app/requirements.txt
-
-EXPOSE 5000
-CMD ["/env/bin/python", "application.py"]
