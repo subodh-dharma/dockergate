81b242697be62c903438b9e857b6d4770740aa38
subodh-dharma <subodh.dharma@gmail.com>
2017-04-14 19:40:10 +0000

    updating code to store exception in a set

--- src/parseJSON.py
+++ src/parseJSON.py
@@ -2,6 +2,8 @@ import os
 import json
 
 rawlibc = ''
+callq_exc = set()
+
 
 with open('./data/libc_f.json','r') as libcjson:
  rawlibc = eval(libcjson.read())
@@ -11,13 +13,18 @@ for key in rawlibc:
  if len(rawlibc[key]["callqs"]) > 0:
   #print 'do something'
   for index in range(0, len(rawlibc[key]["callqs"])):
-   f_callq = rawlibc[key]["callqs"][index]
-   print f_callq
-   if len(rawlibc[f_callq]["callqs"]) > 0:
-    continue
-   else: 
-    rawlibc[key]["syscalls"] = rawlibc[key]["syscalls"] + rawlibc[f_callq]["syscalls"]
-    rawlibc[key]["callqs"].remove(f_callq)
+   try:
+    f_callq = rawlibc[key]["callqs"][index]
+    print f_callq
+    if len(rawlibc[f_callq]["callqs"]) > 0:
+     continue
+    else: 
+     rawlibc[key]["syscalls"] = rawlibc[key]["syscalls"] + rawlibc[f_callq]["syscalls"]
+     rawlibc[key]["callqs"].remove(f_callq)
+   except:
+    #print 'Function not found in SO', f_callq
+    callq_exc.add(f_callq)
+    
  else:
   print 'do nothing'
 
@@ -26,3 +33,5 @@ for key in rawlibc:
 jsonfile = open('./data/libc_out.json', 'w+')
 jsonfile.write(str(rawlibc))
 jsonfile.close() 
+
+print callq_exc
