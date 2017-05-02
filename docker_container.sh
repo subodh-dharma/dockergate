2d02e5aa74dca2f4e470917e3d6f21adff51a722
mohitgoyal2011 <mohit.bits2011@gmail.com>
2017-05-02 14:32:28 +0000

    some more

--- docker_container.sh
+++ docker_container.sh
@@ -2,9 +2,24 @@
 original_name=$1
 filename=${original_name/\//_}
 filename="${filename}.json"
+echo ${filename}
+cmnd="docker run -d -it --security-opt=seccomp=data/policy/${filename} ${1}"
+echo "${cmnd}"
+#sudo tools/systemtap/syscall_chk.stp -c  "$cmnd" | wc -l
+docker pull $1
+docker pull $1:release
+docker run -d -it $1
+docker ps -a > "tmp_status"
 docker run -d -it --security-opt=seccomp=data/policy/${filename} $1
 docker run -d -it --security-opt=seccomp=data/policy/${filename} $1 uname
 docker run -d -it --security-opt=seccomp=data/policy/${filename} $1 echo "hi"
-docker run -d -it --security-opt=seccomp=data/policy/${filename} $1 /bin/bash
+docker run -d -it --security-opt=seccomp=data/policy/${filename} $1 /bin/bash 
 docker run -d -it --security-opt=seccomp=data/policy/${filename} $1 "mkdir -p /home/ubuntu/folder;ls"
+
+docker ps -a >> "tmp_status"
+cat data/policy/${filename} | grep -o SCMP_ACT_ALLOW | wc -l >> "tmp_status"
+
+docker stop $(sudo docker ps -a -q)
+docker rm $(sudo docker ps -a -q)
+docker rmi $(sudo docker images -q)
 echo "Done!"
